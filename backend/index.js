1355b497e004451c7da267d6d5de0eef6bd2a008
Doyeon Guk <54295374+Jenguk42@users.noreply.github.com>
2023-05-07 09:09:01 +1000

    connected chat content to server

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,45 @@
-const apiKey = "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"
+// const apiKey = "";
+require('dotenv').config();
 const { Configuration, OpenAIApi } = require("openai");
-
-// Open server via Express
 const express = require("express");
+var cors = require('cors');
 const app = express();
-
-// Add CORS options
-let corsOptions = {
-  origin: 'https://www.domain.com',
-  credentials: true
-}
-app.use(cors(corsOptions));
-
-// POST method route
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.post("/", function (req, res) {
-  res.send("POST requrest to the homepage")
-});
-
-app.listen(3000)
 
 // Import OpenAI
 const configuration = new Configuration({
-  apiKey: apiKey,
+  apiKey: process.env.API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-async function apiCall() {
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      max_tokens: 100,
-      temperature: 0.8,
-      messages: [
-        {role: "system", content: "You are a very skilled and experienced IELTS course consultant. Your name is Jenny. You have vast knowledge about IELTS study tips based on the official IELTS website and can answer any questions, is helpful, insightful, clever, and friendly. You can also suggest schedules specific to the student's current level. You also gain information about the student's grammar and vocabulary level during the conversation, spotting errors. When suggesting something, you find and attach an existing link on the web that does not return a 404 error. You start the conversation by greeting the student."},
-        {role: "user", content: "You are a very skilled and experienced IELTS course consultant. Your name is Jenny. You have vast knowledge about IELTS study tips based on the official IELTS website and can answer any questions, is helpful, insightful, clever, and friendly. You can also suggest schedules specific to the student's current level. You also gain information about the student's grammar and vocabulary level during the conversation, spotting errors. When suggesting something, you find and attach an existing link on the web that does not return a 404 error. You start the conversation by greeting the student."},
-        {role: "assistant", content: "Hello! Welcome to our IELTS course consultancy service. My name is Jenny, and I am here to assist you with any questions or concerns you may have about studying for the IELTS exam. How can I help you today?"},
-        {role: "user", content: "I need detailed help with my IELTS course."}
-    ],
-    });
-    console.log(completion.data.choices[0].message['content']);    
-}
 
-apiCall();
+// Add CORS 
+// let corsOptions = {
+//   origin: 'https://www.domain.com',
+//   credentials: true
+// }
+app.use(cors());
+
+// Allow POST method
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// POST method route
+app.get("/ieltsConsultant", async function (req, res) {
+  // Call ChatGPT
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    max_tokens: 100,
+    temperature: 0.8,
+    messages: [
+      {role: "system", content: "You are a very skilled and experienced IELTS course consultant. Your name is Jenny. You have vast knowledge about IELTS study tips based on the official IELTS website and can answer any questions, is helpful, insightful, clever, and friendly. You can also suggest schedules specific to the student's current level. You also gain information about the student's grammar and vocabulary level during the conversation, spotting errors. When suggesting something, you find and attach an existing link on the web that does not return a 404 error. You start the conversation by greeting the student."},
+      {role: "user", content: "You are a very skilled and experienced IELTS course consultant. Your name is Jenny. You have vast knowledge about IELTS study tips based on the official IELTS website and can answer any questions, is helpful, insightful, clever, and friendly. You can also suggest schedules specific to the student's current level. You also gain information about the student's grammar and vocabulary level during the conversation, spotting errors. When suggesting something, you find and attach an existing link on the web that does not return a 404 error. You start the conversation by greeting the student."},
+      {role: "assistant", content: "Hello! Welcome to our IELTS course consultancy service. My name is Jenny, and I am here to assist you with any questions or concerns you may have about studying for the IELTS exam. How can I help you today?"},
+      {role: "user", content: "I need detailed help with my IELTS course."}
+    ],
+  });
+
+  let answer = completion.data.choices[0].message['content'];
+  console.log(answer);
+  res.send(answer);
+});
+
+app.listen(3000)
